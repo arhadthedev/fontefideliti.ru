2981eedaa4c2f76b79fbb88ea53177f1bed8939c
Oleg Iarygin <oleg@arhadthedev.net>
2020-09-09 11:04:10 +0300

    Add an environment for Python scripts

---
+++ build.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# build.py - builds the whole site into a public/ directory
+#
+# Copyright (c) 2020 Oleg Iarygin <oleg@arhadthedev.net>
+#
+# Distributed under the MIT software license; see the accompanying
+# file LICENSE.txt or <https://www.opensource.org/licenses/mit-license.php>.
