ca50749635395219b96c50e71d796bfe04c965e8
Oleg Iarygin <oleg@arhadthedev.net>
2021-03-25 22:03:29 +0300

    Replace section_breeders.py with a new script

--- src/build_site.py
+++ src/build_site.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import dogs.section_breeders
 import os
+import sections.breeders
 import sections.main
 import sections.sale
 import sections.shows
@@ -19,27 +19,13 @@ class Input(object):
         return open(full_path, 'r', encoding='utf-8')
 
 
-class Output(object):
-    def __init__(self, base_path):
-        self._base_path = base_path
-
-    def create_file(self, rel_path):
-        full_path = os.path.join(self._base_path, rel_path)
-        if not os.path.exists(os.path.dirname(full_path)):
-            os.makedirs(os.path.dirname(full_path))
-        return open(full_path, 'w', encoding='utf-8')
-
-
 if len(sys.argv) < 2:
     sys.exit('error: output directory path argument is not specified')
 output_base_path = sys.argv[1]
 
 resources = Input('res')
-output = Output(sys.argv[1])
 
-dogs.section_breeders.generate_section(output, resources)
-
-for generator in [sections.main, sections.sale, sections.shows]:
+for generator in [sections.breeders, sections.main, sections.sale, sections.shows]:
     artifacts = generator.get_root_artifact_list(resources)
     for title, path, generator in artifacts:
         extension = 'html' if path.endswith('index') else 'htm'
---
+++ src/sections/breeders.py
@@ -0,0 +1,70 @@
+# -*- coding: UTF-8 -*-
+# breeders.py - generates content of /females and /males
+#
+# Copyright (c) 2021 Oleg Iarygin <oleg@arhadthedev.net>
+#
+# Distributed under the MIT software license; see the accompanying
+# file LICENSE.txt or <https://www.opensource.org/licenses/mit-license.php>.
+
+import yaml
+
+def generate_photos(output_document, resources):
+    output_document.start_container()
+
+    dog_list = yaml.safe_load(resources.get('doglist.yml'))
+
+    path = output_document.get_path()
+    dog_id = path.split('/')[1]
+    name = dog_list[dog_id]['name']
+    output_document.add_raw('<h1>Фото <a href=".">{}</a></h1>'.format(name['gen']))
+
+    photo_list = yaml.safe_load(resources.get('dogphotos.yml'))
+    photos = photo_list.get(dog_id)
+    for photo in photos:
+        preview = photo.get('preview') if photo.get('preview') else 'p'
+        photo['caption'] = photo['caption'] if photo['caption'] != None else ''
+        output_document.add_raw('<a href="../../../img/{p}.jpg" title="{c}" rel="a"><img src="../../../img/{p}-{pr}.jpg" alt="{c}" height="152"></a> '.format(p=photo['path'], pr=preview, c=photo['caption']))
+    output_document.end_container()
+
+
+def generate_videos(output_document, resources):
+    output_document.start_container()
+
+    dog_list = yaml.safe_load(resources.get('doglist.yml'))
+
+    path = output_document.get_path()
+    dog_id = path.split('/')[1]
+    name = dog_list[dog_id]['name']
+
+    youtube_ids = dog_list[dog_id].get('videos')
+    for video_id in youtube_ids:
+        youtube_id, *title = video_id.split(' ', 1)
+        if title:
+            output_document.add_header(3, title[0])
+        output_document.add_raw('<p>')
+        output_document.add_raw('<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'.format(video_id))
+    output_document.end_container()
+
+
+def get_root_artifact_list(resources):
+    section_pages = []
+
+    dog_list = yaml.safe_load(resources.get('doglist.yml'))
+    photo_list = yaml.safe_load(resources.get('dogphotos.yml'))
+    for dog_id, dog_details in dog_list.items():
+        name = dog_details['name']
+        base_url = '{}s/{}/'.format(dog_details['gender'], dog_id)
+
+        photos = photo_list.get(dog_id)
+        if photos:
+            title = "Фото {}".format(name['gen'])
+            page = (title, '{}/photos'.format(base_url), generate_photos)
+            section_pages.append(page)
+
+        youtube_ids = dog_details.get('videos')
+        if youtube_ids:
+            title = "Видео {}".format(name['gen'])
+            page = (title, '{}/videos'.format(base_url), generate_videos)
+            section_pages.append(page)
+
+    return section_pages
