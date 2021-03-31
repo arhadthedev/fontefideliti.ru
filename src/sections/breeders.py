59d9ebcc718b770b5b3e4f60d712d44dd9d73f87
Oleg Iarygin <oleg@arhadthedev.net>
2021-03-31 15:19:29 +0300

    Add script generation for per-dog show lists

--- src/sections/breeders.py
+++ src/sections/breeders.py
@@ -6,6 +6,8 @@
 # Distributed under the MIT software license; see the accompanying
 # file LICENSE.txt or <https://www.opensource.org/licenses/mit-license.php>.
 
+from collections import OrderedDict
+import os
 import yaml
 
 def generate_photos(output_document, resources):
@@ -46,8 +48,117 @@ def generate_videos(output_document, resources):
     output_document.end_container()
 
 
+def filter_shows_for(filtered_dog_id, show_tree):
+    filtered_shows = []
+    for date, eventset in show_tree.items():
+        for event in eventset['events']:
+            for dog_id, dog_details in event['dogs'].items():
+                if dog_id == filtered_dog_id:
+                    element = {}
+                    element['date'] = date
+                    element['rank'] = event['rank']
+                    element['city'] = eventset['city']
+                    element['class'] = dog_details['class']
+                    element['expert'] = event['expert']
+                    element['figurant'] = event.get('figurant', '')
+                    element['place'] = dog_details['place']
+                    element['achievements'] = dog_details.get('achievements', [])
+                    filtered_shows.append(element)
+    return filtered_shows
+
+
+def get_proper_expert_name(last_name, registry):
+    first_name, patronymic = registry[last_name].split(' ')
+    return '{} {}. {}.'.format(last_name, first_name[0], patronymic[0])
+
+
+# Items are printed in the order they specified here
+printable_ranks = OrderedDict()
+printable_ranks['cw'] = 'Победитель класса'
+printable_ranks['лб'] = 'Лучший бэби'
+printable_ranks['лю'] = 'Лучший юниор'
+printable_ranks['лк'] = 'Лучший кобель'
+printable_ranks['jcac'] = 'JunCAC'
+printable_ranks['cac'] = 'CAC'
+printable_ranks['кчф рфлс'] = 'Кандидат в Чемпионы Федерации РФЛС'
+printable_ranks['кчф оанкоо'] = 'Кандидат в Чемпионы Федерации ОАНКОО'
+printable_ranks['чф оанкоо'] = 'Чемпион Федерации ОАНКОО'
+printable_ranks['лпп'] = 'Лучший представитель породы'
+printable_ranks['big 2'] = 'BIG-2'
+printable_ranks['чркф'] = 'Чемпион РКФ'
+printable_ranks['bos'] = 'BOS'
+
+
+def assembly_achievements(place, ranks):
+    place_formatted = ' '.join(place.split(' ', 1))
+    ranks_count = {}
+    for source_rank in ranks:
+        ranks_count.setdefault(source_rank, 0)
+        ranks_count[source_rank] += 1
+
+    def id_to_name(rank_id):
+        count = ranks_count.get(rank_id, 0)
+        if count > 1:
+            return '{}x {}'.format(count, printable_ranks[rank_id])
+        elif count == 1:
+            return printable_ranks[rank_id]
+        else:
+            raise RuntimeError('undefined rank should be filtered by a caller')
+    titles = ', '.join((id_to_name(x) for x in printable_ranks if x in ranks))
+    return '{}, {}'.format(place, titles) if titles else place
+
+def generate_shows(output_document, resources):
+    output_document.start_container(css_classes=['filled'])
+    show_list = yaml.safe_load(resources.get('shows.yml'))
+    dog_list = yaml.safe_load(resources.get('doglist.yml'))
+    experts = yaml.safe_load(resources.get('people.yml'))
+
+    path = output_document.get_path()
+    dog_id = path.split('/')[1]
+    name = dog_list[dog_id]['name']
+    output_document.add_raw('<h1>Результаты выставок <a href=".">{}</a></h1>'.format(name['gen']))
+
+    personal_shows = filter_shows_for(dog_id, show_list)
+    mono_shows = [x for x in personal_shows if x['rank'] == 'монопородная']
+    if mono_shows:
+        output_document.start_container()
+        output_document.add_raw('<h1>Монопородные выставки</h2>')
+        output_document.add_raw('<ol class="chrono-list">')
+        for show in mono_shows:
+            achievements = assembly_achievements(show['place'], show['achievements'])
+            expert = get_proper_expert_name(show['expert'], experts)
+            if show['figurant']:
+                figurant_name = get_proper_expert_name(show['figurant'], experts)
+                figurant = ', фигурант {}'.format(figurant_name)
+            else:
+                figurant = ''
+            output_document.add_raw('<li>')
+            output_document.add_date(show['date'])
+            output_document.add_raw(' Монопородная выставка КЧК, г. {ci}, класс {cl}, <strong>{a}</strong> (эксперт {e}{f}).'.format(ci=show['city'], cl=show['class'], a=achievements, e=expert, f=figurant))
+        output_document.add_raw('</ol>')
+        output_document.end_container()
+
+    nonmono_shows = [x for x in personal_shows if x['rank'] != 'монопородная']
+    if nonmono_shows:
+        output_document.start_container()
+        output_document.add_raw('<h1>Всепородные выставки</h2>')
+        output_document.add_raw('<ol class="chrono-list">')
+        for show in nonmono_shows:
+            achievements = assembly_achievements(show['place'], show['achievements'])
+            expert = get_proper_expert_name(show['expert'], experts)
+            output_document.add_raw('<li>')
+            output_document.add_date(show['date'])
+            output_document.add_raw(' {s}, г. {ci}, класс {cl}, <strong>{a}</strong> (эксперт {e}).'.format(s=show['rank'].capitalize().replace(' чркф', ' ЧРКФ'), ci=show['city'], cl=show['class'], a=achievements, e=expert))
+        output_document.add_raw('</ol>')
+        output_document.end_container()
+    output_document.end_container()
+
+    output_document.end_container()
+
+
 def generate_index(output_document, resources):
     dog_list = yaml.safe_load(resources.get('doglist.yml'))
+    show_list = yaml.safe_load(resources.get('shows.yml'))
 
     path = output_document.get_path()
     dog_id = path.split('/')[1]
@@ -155,6 +266,12 @@ def get_root_artifact_list(resources):
         if youtube_ids:
             title = "Видео {}".format(name['gen'])
             page = (title, '{}/videos'.format(base_url), generate_videos)
+            section_pages.append(page)
+
+        shows_url = '{}/shows'.format(base_url)
+        if not os.path.exists(shows_url):
+            title = "Результаты выставок {}".format(name['gen'])
+            page = (title, shows_url, generate_shows)
             section_pages.append(page)
 
     section_pages.append(('Производители', 'males/index', generate_list))
