01d965b624e36092e9019236b6e987d8a6615c58
Oleg Iarygin <oleg@arhadthedev.net>
2021-04-02 21:12:15 +0300

    Get rid of the only remaining dependency on Jekill

--- src/sections/shows.py
+++ src/sections/shows.py
@@ -6,6 +6,7 @@
 # Distributed under the MIT software license; see the accompanying
 # file LICENSE.txt or <https://www.opensource.org/licenses/mit-license.php>.
 
+import re
 import yaml
 
 def generate_shows(output_document, resources):
@@ -23,7 +24,32 @@ def generate_shows(output_document, resources):
         output_document.add_raw('</a>')
         output_document.end_container()
 
+
+def generate_year_page(output_document, resources):
+    path = output_document.get_path()
+    year = path.split('/')[1]
+    source = resources.get('../_shows/{}'.format(year)).read()
+
+    end_of_frontmatter = source.find('---', 1) + 3
+    source = source[end_of_frontmatter:]
+
+    first_image = source.find('{%')
+    output_document.add_raw(source[:first_image])
+
+    matches = re.findall(r'{% include photo.html path="([^"]*)" title="([^"]*)"([^{]*)', source)
+    for path, title, rest in matches:
+        output_document.add_image(path, title, 'h', 152, is_clickable=True)
+        output_document.add_raw(rest)
+
+
 def get_root_artifact_list(resources):
     section_pages = [('Выставки', 'shows/index', generate_shows)]
+
+    per_year_list_raw = resources.get('show_years.yml')
+    per_year_list = yaml.safe_load(per_year_list_raw)
+    for year_entry in per_year_list:
+        year = year_entry['год']
+        section_pages.append(('Выставки {} года'.format(year), 'shows/{}'.format(year), generate_year_page))
+
     return section_pages
 
