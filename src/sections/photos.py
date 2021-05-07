554a43362c22e15db8ca93d8e26593346c9af2d1
Oleg Iarygin <oleg@arhadthedev.net>
2021-05-07 18:04:36 +0300

    Convert manual photos.htm gathering to data-driven

--- src/sections/photos.py
+++ src/sections/photos.py
@@ -12,11 +12,9 @@ def generate_photos(output, database, extra):
     output.start_container()
     output.add_header(1, 'Фотографии всех наших собак')
 
-    dog_list = database['resources'].get_yaml('dogphotos.yml')
-    photo_list = dog_list['$']
-    for photo in photo_list:
-        photo_ = database['photos'].get_for_id(photo['path'])
-        output.add_image(photo_.get_id(), photo['caption'] if photo['caption'] else photo_.get_caption(), 'h', 152, True, photo_.get_image())
+    photos = database['photos'].get_card_assignation('allphotos')
+    for photo in photos:
+        output.add_image('',  '', 'h', 152, True, photo.get_image())
         output.add_plain(' ')
     output.end_container()
 
